d766325b20f246600d38e660811736ea44ffef3c
huihu <442001189@qq.com>
2020-02-24 13:22:37 +0800

    fix ThunderAtronach states SpellNatureDamageMulti to SpellThunderDamageMulti

--- src/main/java/com/robertx22/database/stat_types/traits/atronachs/ThunderAtronach.java
+++ src/main/java/com/robertx22/database/stat_types/traits/atronachs/ThunderAtronach.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.robertx22.database.stat_mods.multi.elemental.damage.SpellNatureDamageMulti;
+import com.robertx22.database.stat_mods.multi.elemental.damage.SpellThunderDamageMulti;
 import com.robertx22.database.stats.IAffectsOtherStats;
 import com.robertx22.database.stats.StatMod;
 import com.robertx22.database.stats.Trait;
@@ -24,7 +25,7 @@ public class ThunderAtronach extends Trait implements IAffectsOtherStats {
 
     @Override
     public List<StatMod> getStats() {
-	return Arrays.asList(new SpellNatureDamageMulti());
+	return Arrays.asList(new SpellThunderDamageMulti());
 
     }
 
